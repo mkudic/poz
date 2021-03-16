6e5c5e143b813351eed0f23e70518aae08e522fc
Mensura Kudic <mkudic1@gmail.com>
2021-03-16 12:46:16 +0100

    initial commit

---
+++ src/ba/unsa/etf/rpr/predavanje02/MojaPrvaKlasa.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.predavanje02;
+
+public class MojaPrvaKlasa {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Helloworld");
+    }
+}
